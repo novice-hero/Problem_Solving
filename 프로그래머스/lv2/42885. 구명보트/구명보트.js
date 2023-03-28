df7e373a0ae55603957cb1738421eb713ad15691
KimYoungWoong <77836614+novice-hero@users.noreply.github.com>
2023-03-28 19:28:41 +0900

    [level 2] Title: 구명보트, Time: 11.41 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -12,22 +12,4 @@ function solution(people, limit) {
         answer++;
     }
     return answer;
-    // let answer = 0;
-    // const sortPeople = people.sort((a,b)=>a-b);
-    // while (sortPeople.length) {
-    //     if (sortPeople.length > 1) {
-    //         if (sortPeople.at(0) + sortPeople.at(-1) <= limit) {
-    //             sortPeople.shift();
-    //             sortPeople.pop();
-    //             answer++;
-    //         } else {
-    //             sortPeople.pop();
-    //             answer++;
-    //         }
-    //     } else {
-    //         sortPeople.pop();
-    //         answer++;
-    //     }
-    // }
-    // return answer;
 }
