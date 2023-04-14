ec2119294eabf449c9fbd5ba8b825609492659a4
KimYoungWoong <77836614+novice-hero@users.noreply.github.com>
2023-04-14 10:49:29 +0900

    [level 2] Title: 피보나치 수, Time: 4.48 ms, Memory: 34.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    const answer = [];
+    for (let i=0;i<=n;i++) {
+        if(i===0) answer.push(i);
+        if(i===1) answer.push(i);
+        else if (i >= 2) {
+            let temp = answer[answer.length-1]+answer[answer.length-2];
+            answer.push(temp%1234567);
+        }
+    }
+    return answer[n];
+}
