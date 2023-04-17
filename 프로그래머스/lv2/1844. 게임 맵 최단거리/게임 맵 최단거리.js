6069bd85823cd7cb1c3e4abb1b4bf74b387aa475
KimYoungWoong <77836614+novice-hero@users.noreply.github.com>
2023-04-17 15:40:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.47 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -2,18 +2,19 @@ function solution(maps) {
     const moves = [[0,1],[0,-1],[1,0],[-1,0]];
     const WIDTH = maps[0].length;
     const HEIGHT = maps.length;
-    const visited = Array.from({length: HEIGHT}, ()=>Array(WIDTH).fill(-1))
+    const visited = Array.from({length: HEIGHT}, ()=>Array(WIDTH).fill(0))
     
     const BFS = (a, b) => {
         const q = [];
         q.push([a, b]);
+        visited[a][b] = 1;
         while (q.length) {
             const [r, c] = q.shift();
             for (const move of moves) {
                 const nr = r + move[0];
                 const nc = c + move[1];
                 if (nr < 0 || nr >= HEIGHT || nc < 0 || nc >= WIDTH || maps[nr][nc] === 0 || 
-                    visited[nr][nc] !== -1) continue;
+                    visited[nr][nc] !== 0) continue;
                 visited[nr][nc] = visited[r][c] + 1;
                 q.push([nr,nc]);
             }
@@ -21,6 +22,5 @@ function solution(maps) {
     }
     BFS(0,0);
     const answer = visited[HEIGHT-1][WIDTH-1];
-    
-    return answer === -1 ? answer : answer+2;
+    return answer === 0 ? -1 : answer;
 }
