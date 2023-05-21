dc8fb2b33e3b8779f2619b0da5890953b587b9f1
KimYoungWoong <77836614+novice-hero@users.noreply.github.com>
2023-05-21 19:54:07 +0900

    [Silver II] Title: 연속합, Time: 92 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,7 +1,5 @@
 n = int(input())
 arr = list(map(int, input().split()))
-
-for i in range(1, n):
-  arr[i] = max(arr[i], arr[i-1]+arr[i])
-
+for i in range(1, len(arr)):
+    arr[i] = max(arr[i], arr[i - 1] + arr[i])
 print(max(arr))
