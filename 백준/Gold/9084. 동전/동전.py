8284224c11634725186805a2a3f0dc5ef85e2eb5
KimYoungWoong <77836614+novice-hero@users.noreply.github.com>
2023-05-30 19:09:55 +0900

    [Gold V] Title: 동전, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -7,8 +7,7 @@ for _ in range(T):
     dp[0] = 1
 
     for coin in coins:
-        for i in range(M + 1):
-            if i >= coin:
-                dp[i] += dp[i - coin]
+        for i in range(coin, M + 1):
+            dp[i] += dp[i - coin]
 
     print(dp[-1])
