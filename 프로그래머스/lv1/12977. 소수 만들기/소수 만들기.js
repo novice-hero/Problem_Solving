6058f2c22184b0a4f93719342f97de918941ce0a
KimYoungWoong <77836614+novice-hero@users.noreply.github.com>
2023-10-04 23:05:40 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 30.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,21 @@
+function solution(nums) {
+    let answer = 0;
+    for (let i=0;i<nums.length;i++) {
+        for (let j=i+1;j<nums.length;j++) {
+            for (let k=j+1;k<nums.length;k++) {
+                let sum = nums[i]+nums[j]+nums[k];
+                if (isPrime(sum)) answer++;
+            }
+        }   
+    }
+    return answer;
+}
+
+function isPrime(n){
+    for(let i = 2; i <= Math.sqrt(n); i++){
+        if(n%i === 0){
+            return false;
+        }
+    }  
+    return true;    
+}
